f5fd0480a453a81512979e592d899d71553f0e0c
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-01-29 23:54:49 -0500

    finished bankaccount exercise, added comments, added withdraw, getting total balance

--- BankAccountSimulator/src/com/naldojesse/bankaccountsim/BankAccount.java
+++ BankAccountSimulator/src/com/naldojesse/bankaccountsim/BankAccount.java
@@ -4,20 +4,26 @@ package com.naldojesse.bankaccountsim;
 import java.math.BigDecimal;
 import java.util.concurrent.ThreadLocalRandom;
 
+//● Users should not be able to set any of the attributes from the class.
 public class BankAccount {
 	private String accountNum;
 	private BigDecimal checkingsBal;
 	private BigDecimal savingsBal;
 	private static int numAccounts = 0;
-	private static BigDecimal totalAllAccounts = null;
+	private static BigDecimal totalAllAccounts = new BigDecimal(0);
 	
 	public static String digits = "0123456789";
 	
+	//will take a double and convert and round to BigDecimal
+	private BigDecimal currConverter(double amount) {
+		return new BigDecimal(amount).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+	}
+	
+	//● Create a private method that returns a random ten digit account number.
 	private void acctGen() {
 		String str = "";
 		for (int i = 0; i < 10; i++) {
 			int randomNum = ThreadLocalRandom.current().nextInt(0, 10);
-//			
 			str += randomNum;
 		}
 		System.out.println("New acct number: " + str);
@@ -25,6 +31,11 @@ public class BankAccount {
 		
 	}
 	
+	public static BigDecimal getAllAccountsTotal() {
+		return totalAllAccounts;
+	}
+	
+	//● In the constructor, be sure to increment the account count
 	BankAccount() {
 		acctGen();
 		numAccounts++;
@@ -33,30 +44,59 @@ public class BankAccount {
 		savingsBal = new BigDecimal(0);
 	}
 	
-	public BigDecimal getCheckingsBal() {
-		return checkingsBal;
+	//● Create a getter method for the user's checking account balance.
+	public String getCheckingsBal() {
+		return "$" + checkingsBal;
 	}
 	
-	public BigDecimal getSavingsBal() {
-		return savingsBal;
+	//● Create a getter method for the user's saving account balance
+	public String getSavingsBal() {
+		return "$" + savingsBal;
 	}
 	
+	//● Create a method that will allow a user to deposit money into either the checking or saving, be sure to add to total amount stored
 	public void deposit(String str, double amount) {
-		BigDecimal converted = new BigDecimal(amount).setScale(2,  BigDecimal.ROUND_HALF_EVEN);
+		BigDecimal cAmount = currConverter(amount);
 		if (str == "savings") {
 			//BigDecimal is immutable so you need to assign to local var and replace it
-			BigDecimal result = savingsBal.add(converted);
-			savingsBal = result;
-//			System.out.println("Savings");
-//			System.out.println(savingsBal);
+			savingsBal = savingsBal.add(cAmount);
 		} else {
-			BigDecimal result = checkingsBal.add(converted);
-			checkingsBal = result;
-			
-//			System.out.println("Checkings");
-//			System.out.println(checkingsBal);
+			checkingsBal = checkingsBal.add(cAmount);
+		}
+		totalAllAccounts = totalAllAccounts.add(cAmount);
+		
+	}
+	
+	//● Create a method to withdraw money from one balance. Do not allow them to withdraw money if there are insufficient funds.
+	public String withdraw(String str, double amount) {
+		BigDecimal cAmount = currConverter(amount);
+		if(str == "savings") {
+//			 a.compareTo(b) returns a number greater than zero if a > b, 0 if a == b, and less than zero if a < b.
+			int cresult = savingsBal.compareTo(cAmount);
+			if (cresult < 0) {
+				return "Insufficient Funds!";
+			} else {
+				savingsBal = savingsBal.subtract(cAmount);
+				totalAllAccounts = totalAllAccounts.subtract(cAmount);
+				return "Withdraw is complete, your total savings balance is now $" + savingsBal;
+			}
+		} else {
+			int cresult = checkingsBal.compareTo(cAmount);
+			if (cresult < 0) {
+				return "Insufficient Funds";
+			} else {
+				checkingsBal = checkingsBal.subtract(cAmount);
+				totalAllAccounts = totalAllAccounts.subtract(cAmount);
+				return "Withdraw is complete, your total checkings balance is now $" + checkingsBal;
+				
+			}
 		}
 		
+	}
+	
+	public String getTotalBalance() {
+		BigDecimal totalBalance = savingsBal.add(checkingsBal);
+		return "Your total balance is : $" + totalBalance;
 	}
 	
 	
