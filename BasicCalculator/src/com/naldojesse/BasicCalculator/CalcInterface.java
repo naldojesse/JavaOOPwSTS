c3e861c089b2dcb69bc9fbe441d144ed51aa9bf0
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-01-31 20:08:36 -0800

    added interface with needed methods for calculating and getting results, added setter methods for operands

---
+++ BasicCalculator/src/com/naldojesse/BasicCalculator/CalcInterface.java
@@ -0,0 +1,7 @@
+package com.naldojesse.BasicCalculator;
+
+public interface CalcInterface {
+	
+	void performOperation();
+	double getResults();
+}
--- BasicCalculator/src/com/naldojesse/BasicCalculator/Calculator.java
+++ BasicCalculator/src/com/naldojesse/BasicCalculator/Calculator.java
@@ -1,9 +1,48 @@
 package com.naldojesse.BasicCalculator;
 
-public class Calculator implements java.io.Serializable{
+public class Calculator implements java.io.Serializable, CalcInterface{
+	private double operandOne;
+	private double operandTwo; 
+	private String operation;
+	private double results;
+	
 	
 	public Calculator() {
-		
+		operandOne = 0;
+		operandTwo = 0;
+		operation = "+";
+		results = 0;
+	}
+	
+	public Calculator(double operandOne, double operandTwo, String operation) {
+		this.operandOne = operandOne;
+		this.operandTwo = operandTwo;
+		this.operation = operation;
+	}
+	
+	public void setOperandOne(double operandOne) {
+		this.operandOne = operandOne;
+	}
+	
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	
+	public void setOperandTwo(double operandTwo) {
+		this.operandTwo = operandTwo;
+	}
+
+	
+	public void performOperation() {
+		if (operation == "+") {
+			results = operandOne + operandTwo;
+		} else {
+			results = operandOne - operandTwo;
+		}
+	}
+	
+	public double getResults() {
+		return results;
 	}
 	
 }
