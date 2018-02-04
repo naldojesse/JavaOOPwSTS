3612b2ea1268c5aaf3de6a5c1fb651b9f1c3cd0c
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-02-04 15:37:02 -0800

    finished calculator version 2

--- CalculatorII/src/com/naldojesse/calculatorII/Calculator.java
+++ CalculatorII/src/com/naldojesse/calculatorII/Calculator.java
@@ -2,26 +2,41 @@ package com.naldojesse.calculatorII;
 import java.util.ArrayList;
 
 public class Calculator implements java.io.Serializable, CalcInterface{
-    private double results = 0;
-//    private String currOperand = "";
-    private ArrayList<Object> equation = new ArrayList<Object>();
+    private double results;
+      private ArrayList<Double> numbers = new ArrayList<>();
+      private ArrayList<String> operands = new ArrayList<>();
+
+    public Calculator() {
+    }
+
+    private void calculate() {
+        results = numbers.get(0);
+        System.out.println(results);
+        System.out.println(operands.size());
+        for (int i = 0; i < operands.size(); i++) {
+            if (operands.get(i).equals("+")) {
+                results += numbers.get(i+1);
+            } else if (operands.get(i).equals("-")) {
+                results -= numbers.get(i+1);
+            } else if (operands.get(i).equals("*")) {
+                results *= numbers.get(i+1);
+            } else if (operands.get(i).equals("/")) {
+                results /= numbers.get(i+1);
+            }
+        }
+    }
 
     public Calculator performOperation(String operand) {
-//        currOperand = operand;
-        equation.add(operand):
+        if (operand.equals("=")) {
+            calculate();
+        } else {
+            operands.add(operand);
+        }
         return this;
     }
 
     public Calculator performOperation(double number) {
-//        if (currOperand.equals("+")) {
-//            results += number;
-//        } else if (currOperand.equals("-")) {
-//            results -= number;
-//        } else if (currOperand.equals("*")) {
-//            results *= number;
-//        } else if (currOperand.equals("/")) {
-//            results = results / number;
-//        }
+        numbers.add(number);
         return this;
     }
 
--- CalculatorII/src/com/naldojesse/calculatorII/CalculatorTest.java
+++ CalculatorII/src/com/naldojesse/calculatorII/CalculatorTest.java
@@ -1,4 +1,15 @@
 package com.naldojesse.calculatorII;
+//
+//The client has requested a change in spec: they need the calculator to process multiple values, as well as have some more complex abilities. They need the possibility for you to add several numbers in a row: this requires a change in our interface as we can't be sure how many total operands the client will be using. Now they expect this to work:
+//        ● performOperation(10.5)
+//        ● performOperation("+")
+//        ● performOperation(5.2)
+//        ● performOperation("*")
+//        ● performOperation(10)
+//        ● performOperation("=")
+//        ● getResults()
+//        where the result would be 62.5. They want to be able to do this for an arbitrary number of numbers, as well as add in the capability to multiply and divide. You can assume that all inputs will be valid. Notice that the calculator must correctly perform the order of operations.
+
 
 public class CalculatorTest {
 
@@ -6,8 +17,8 @@ public class CalculatorTest {
         Calculator c1 = new Calculator();
 
         double res = c1.performOperation(10.5).performOperation("+").performOperation(5.2).performOperation("*").performOperation(10).performOperation("=").getResults();
-//        System.out.println("hello");
-        System.out.println(res);
+        System.out.println("Answer must equal 157");
+        System.out.println("Calculation: " + res);
 
     }
 
